b8fef2486625778faa96b8922e6296f917b6e3d0
Peter Varga NTB <petervarga9116@live.com>
2017-09-28 21:12:19 +0200

    parallax implemented

--- ClubArcada/ClubArcada.Web/Scripts/site.js
+++ ClubArcada/ClubArcada.Web/Scripts/site.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(document).ready(function () {
+    $('.parallax-window').parallax({ imageSrc: 'http://localhost/ClubArcada.Web/Content/images/px01.jpg' });
+    SetWelcomeHeignt();
+});
+
+$(window).resize(function () {
+    SetWelcomeHeignt();
+});
+
+
+$(window).scroll(function (event) {
+    SetWelcomeHeignt();
+});
+
+function SetWelcomeHeignt() {
+    var section = $('#sc-welcome');
+    var height = $(window).height();
+    section.css('height', height + "px");
+};
