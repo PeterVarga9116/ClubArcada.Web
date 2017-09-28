9df0f7f624168ffb0ed90b29a6fc8a47d862a8e1
Peter Varga NTB <petervarga9116@live.com>
2017-09-28 20:17:22 +0200

    basic str

---
+++ ClubArcada/ClubArcada.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace ClubArcada.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/parallax.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/site.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
